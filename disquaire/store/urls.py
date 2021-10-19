e1a9e9fc5b0fc9e6eec09f252062f85da94843fe
Yoann <yoann.deblois@gmail.com>
2021-10-19 19:20:50 +0200

    func: added artist description page

--- disquaire/store/urls.py
+++ disquaire/store/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.listing),  # "/store" will call the method "index" in "views.py"
     path('<int:album_id>', views.detail),
     path('search/', views.search),
+    path('artist/<int:artist_id>', views.artist)
 ]

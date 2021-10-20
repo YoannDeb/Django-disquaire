8b858b6745d45c150e69fadf95254fdf12fb5f60
Yoann <yoann.deblois@gmail.com>
2021-10-20 16:38:18 +0200

    func: added names for url paths and app_name (namespace)

--- disquaire/store/urls.py
+++ disquaire/store/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views  # import views so we can use them in urls.
 
-
+app_name = 'store'
 urlpatterns = [
-    path('', views.listing),  # "/store" will call the method "index" in "views.py"
-    path('<int:album_id>', views.detail),
-    path('search/', views.search),
-    path('artist/<int:artist_id>', views.artist)
+    path('', views.listing, name='listing'),  # "/store" will call the method "index" in "views.py"
+    path('<int:album_id>', views.detail, name='detail'),
+    path('search/', views.search, name='search'),
+    path('artist/<int:artist_id>', views.artist, name='artist')
 ]

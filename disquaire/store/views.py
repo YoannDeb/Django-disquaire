e58b5960d00d0170bcaa162db6a6bdb42cfc216f
Yoann <yoann.deblois@gmail.com>
2021-10-20 16:38:47 +0200

    func: updated to use render and templates

--- disquaire/store/views.py
+++ disquaire/store/views.py
@@ -1,39 +1,50 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Album, Artist, Contact, Booking
-
-
-def artist(request, artist_id):
-    albums = Album.objects.filter(artists=artist_id)
-    artist = Artist.objects.get(pk=artist_id)
-    formatted_albums = ["<li>{}</li>".format(f"<a href=../{album.id}>{album.title}</a>") for album in albums]
-    # message = f"{artist.name}"
-    if formatted_albums:
-        message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    else:
-        message = f"No album for {artist.name} yet"
-    return HttpResponse(message)
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
     # formatted_albums = ["<li>{}</li>".format(f"<a href=store/{album.id}>{album.title}</a>") for album in albums]
     context = {'albums': albums}
-    template = loader.get_template('store/index.html')
-    return HttpResponse(template.render(context, request=request))
+    return render(request, 'store/index.html', context)
 
 
 def listing(request):
-    albums = ["<li>{}</li>".format(album['name']) for album in Album.objects.all()]
-    message = """<ul>{}</ul>""".format("\n".join(albums))
-    return HttpResponse(message)
+    albums = Album.objects.all().order_by('-created_at')
+    context = {
+        'albums': albums
+    }
+    return render(request, 'store/listing.html', context)
 
 
 def detail(request, album_id):
-    album = Album.objects.get(pk=album_id)
-    artists = " ".join([f"<a href=artist/{artist.id}>{artist.name}</a>" for artist in album.artists.all()])
-    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
-    return HttpResponse(message)
+    album = get_object_or_404(Album, pk=album_id)
+    artists = album.artists.all()
+    context = {
+        'album_title': album.title,
+        'artists': artists,
+        'album_id': album.id,
+        'thumbnail': album.picture
+    }
+    return render(request, 'store/detail.html', context)
+
+
+def artist(request, artist_id):
+    albums = Album.objects.filter(artists=artist_id)
+    artist = get_object_or_404(Artist, pk=artist_id)
+    title = f'Tous les disques de {artist.name}'
+    # formatted_albums = ["<li>{}</li>".format(f"<a href=../{album.id}>{album.title}</a>") for album in albums]
+    # message = f"{artist.name}"
+    if not albums:
+        albums = [f"No album for {artist.name} yet"]
+    context = {
+        'albums': albums,
+        'artist': artist.name,
+        'title': title
+    }
+    return render(request, 'store/artist.html', context)
 
 
 def search(request):
@@ -47,13 +58,15 @@ def search(request):
     if not albums.exists():
         albums = Album.objects.filter(artists__name__icontains=query)
 
-    if not albums.exists():
-        message = "Misère de misère, nous n'avons trouvé aucun résultat !"
-    else:
-        albums = ["<li>{}</li>".format(album.title) for album in albums]
-        message = """
-            Nous avons trouvé les albums correspondant à votre requête ! Les voici :
-            <ul>{}</ul>
-        """.format("</li><li>".join(albums))
+    # if not albums.exists():
+    #     message = "Misère de misère, nous n'avons trouvé aucun résultat !"
 
-    return HttpResponse(message)
+    if query:
+        title = f"Résultats pour la requête {query}"
+    else:
+        title = "Tous les albums"
+    context = {
+        'albums': albums,
+        'title': title
+    }
+    return render(request, 'store/search.html', context)

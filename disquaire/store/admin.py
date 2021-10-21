6d9f42cb22bca25a478f7bb4458ea35b2420a2bd
Yoann <yoann.deblois@gmail.com>
2021-10-21 18:45:28 +0200

    func: added admin user and store management

--- disquaire/store/admin.py
+++ disquaire/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Album, Artist, Contact, Booking
 
-# Register your models here.
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Contact)
+admin.site.register(Booking)
